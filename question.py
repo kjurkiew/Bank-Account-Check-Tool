f82fe8d4856548ca814dbe3b842d117964083ff5
Krzysztof <35111684+kjurkiew@users.noreply.github.com>
2023-08-30 18:54:50 +0200

    Update question.py

--- question.py
+++ question.py
@@ -3,57 +3,57 @@
 import requests
 from openpyxl import load_workbook, Workbook
 
-workbooky = load_workbook(filename='list_test.xlsx')
-sheet = workbooky.active
-a = 2 # first verse
-b = 10 # last verse
-
-for value in sheet.iter_cols(min_row=a,
-                             max_row=b,
-                            min_col=7,
-                            max_col=7,
-                        values_only=True):
-    nips = value
-
-nipk = tuple(map(str, nips))
-
-bank = '2030' # BNP PARIBAS
-nipy = nipk
-data = '2020-01-24'
-lista_bnp = []
+BASE_URL = 'https://wl-api.mf.gov.pl/api/search/nip/'
 
 # Getting a bank account number
-def get_bank_number(nip, data):
-    r = requests.get('https://wl-api.mf.gov.pl/api/search/nip/'+nip+'?date='+data)
-    r = r.json()
-    r = r ['result']
-    acc = r ['subject']
-    if acc == None:
-        acc = ['75249000050000453073876066']
-    else:
-        acc = acc ['accountNumbers']
-    return acc
+def get_bank_account_numbers(nip, date):
+    url = f'{BASE_URL}{nip}?date={date}'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        account_numbers = data.get('result', {}).get('subject', {}).get('accountNumbers', [])
+        return account_numbers
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data for NIP {nip}: {e}")
+        return []
 
 # Checking whether any of the downloaded account numbers belong to BNP
-def is_bnp(nip, acc, list_bnp):
-    for x in acc:
-        if x[2:6] == bank:
-            list_bnp.append(nip)
-    return list_bnp
+def is_bnp_account(account_number, bank_code):
+    return account_number.startswith(bank_code)
 
-for nip in nipy:
+def main():
+    workbook_filename = 'list_test.xlsx'
+    date = '2020-01-24'
+    bank_code = '2030' # BNP PARIBAS
+    output_filename = f'wyniki_{date}.xlsx'
 
-    acc = get_bank_number(nip, data)
-    is_bnp(nip, acc, lista_bnp)
+    try:
+        workbook = load_workbook(filename=workbook_filename)
+        sheet = workbook.active
+        start_row = 2
+        end_row = 10
 
-print(lista_bnp)
+        nips = [str(value[0]) for value in sheet.iter_cols(min_row=start_row,
+                                                           max_row=end_row,
+                                                           min_col=7,
+                                                           max_col=7,
+                                                           values_only=True)]
 
-book = Workbook()
-sheet_2 = book.active
-r = 1
-for x in lista_bnp:
-    sheet_2.cell(row=r, column=1).value = x
-    r += 1
+        lista_bnp = [nip for nip in nips if any(is_bnp_account(acc, bank_code) for acc in get_bank_account_numbers(nip, date))]
 
-name = 'wyniki' + str(a) + '_' + str(b)
-book.save(name + ".xlsx")
+        if lista_bnp:
+            result_workbook = Workbook()
+            result_sheet = result_workbook.active
+            for i, nip in enumerate(lista_bnp, start=1):
+                result_sheet.cell(row=i, column=1).value = nip
+            result_workbook.save(output_filename)
+            print(f"Results saved to {output_filename}")
+        else:
+            print("No results found.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
